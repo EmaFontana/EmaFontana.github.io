2fbc0dfbf96184a843df4d05fe02799045eabcdf
EmaFontana <126437832+EmaFontana@users.noreply.github.com>
2023-02-27 01:02:52 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,18 @@
+var check = document.querySelector('#check');
+
+document.getElementById("nav_inicio").onclick = function () {
+    check.checked = false;
+}
+document.getElementById("nav_estudio").onclick = function () {
+    check.checked = false;
+}
+document.getElementById("nav_experiencia").onclick = function () {
+    check.checked = false;
+}
+document.getElementById("nav_complemento").onclick = function () {
+    check.checked = false;
+}
+document.getElementById("nav_contacto").onclick = function () {
+    check.checked = false;
+}
+
